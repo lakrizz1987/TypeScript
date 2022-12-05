872aac950a208358f4fb0f0ef4c677a60801c2ab
Ivaylo <ignatov.ivaylo@gmail.com>
2022-12-05 16:11:07 +0200

    type array

--- TsBasicsAndBasicsTypes/app.js
+++ TsBasicsAndBasicsTypes/app.js
@@ -1,5 +1,8 @@
 var person = {
     name: 'Ivo',
-    age: 34
+    age: 34,
+    hobies: ['Sport', 'TV'],
+    mix: [1, 'play']
 };
 console.log(person.name);
+person.hobies.forEach(function (hobby) { return console.log(hobby.toUpperCase()); });
--- TsBasicsAndBasicsTypes/app.ts
+++ TsBasicsAndBasicsTypes/app.ts
@@ -1,9 +1,15 @@
 const person: {
     name: string;
     age: number
+    hobies: string[];
+    mix: any[]
 } = {
     name: 'Ivo',
-    age: 34
+    age: 34,
+    hobies: ['Sport', 'TV'],
+    mix: [1, 'play']
 }
 
 console.log(person.name)
+
+person.hobies.forEach(hobby => console.log(hobby.toUpperCase()));
