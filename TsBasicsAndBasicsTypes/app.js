613699b13b07445b61c47784705966e932437b0c
Ivaylo <ignatov.ivaylo@gmail.com>
2022-12-05 15:27:47 +0200

    basic types - number

---
+++ TsBasicsAndBasicsTypes/app.js
@@ -0,0 +1,6 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var a = 2;
+var b = 3;
+console.log(add(a, b));
---
+++ TsBasicsAndBasicsTypes/app.ts
@@ -0,0 +1,9 @@
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+const a = 2;
+const b = 3;
+
+console.log(add(a, b));
+
