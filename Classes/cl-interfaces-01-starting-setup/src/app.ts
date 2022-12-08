1c509221a96787eea88fb3a8d6d76deef5b04d1d
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-12-08 18:27:07 +0200

    private properties

--- Classes/cl-interfaces-01-starting-setup/src/app.ts
+++ Classes/cl-interfaces-01-starting-setup/src/app.ts
@@ -2,7 +2,8 @@
 class Department {
     name: string;
     number: number;
-    
+    private employees: string[] = [];
+
     constructor(name: string, number: number) {
         this.name = name;
         this.number = number;
@@ -11,8 +12,24 @@ class Department {
     print() {
         console.log('Hello from ' + this.name)
     }
+
+    addEmployee(name: string) {
+        this.employees.push(name);
+    }
+
+    printEmployeesInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting', 1);
-console.log(accounting)
+console.log(accounting);
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Ivo');
+
+// accounting.employees[2] = 'Pesho' - When is private is only accesible from inside of class
+
+accounting.printEmployeesInfo()
 accounting.print();
