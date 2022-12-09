142bdf2e5f3a3247e42dfa2cf56fed03b0d7ae1d
Ivaylo <ignatov.ivaylo@gmail.com>
2022-12-09 10:43:03 +0200

    classes and interface

--- Classes/cl-interfaces-01-starting-setup/src/app.ts
+++ Classes/cl-interfaces-01-starting-setup/src/app.ts
@@ -2,16 +2,34 @@ interface Person {
     name: string;
     age: number;
 
-    greet(phrase:string): void;
+    greet(phrase: string): void;
+}
+
+class User implements Person {
+    name: string;
+    age: number;
+
+    constructor(n: string, age: number) {
+        this.name = n;
+        this.age = age;
+    }
+
+    greet(phrase: string): void {
+        console.log(`${phrase} ${this.name} on ${this.age}`);
+    }
 }
 
 const user1: Person = {
     name: 'Ivo',
     age: 34,
-    greet(phrase:string){
+    greet(phrase: string) {
         console.log(`${phrase} ${this.name}`);
     }
 }
 
+const user2: Person = new User('Moni', 34);
+
 user1.name = "Ivo"
-user1.greet('Hello')
+user1.greet('Hello');
+
+user2.greet('Hey aa')
