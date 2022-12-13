16acd94325b72bc4c7b6fcda310ea72457948a7c
Ivaylo <ignatov.ivaylo@gmail.com>
2022-12-13 11:48:22 +0200

    generics functions

--- Generics/generics-01-starting-setup/src/app.ts
+++ Generics/generics-01-starting-setup/src/app.ts
@@ -1,1 +1,8 @@
 // Code goes here!
+
+function combine<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+let result = combine<{ name: string }, { age: number }>({ name: 'Ivo' }, {age:30});
+console.log(result)
