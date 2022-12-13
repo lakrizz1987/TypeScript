53de5ab2816474a0167ee14f0748975e38b7f48c
Ivaylo <ignatov.ivaylo@gmail.com>
2022-12-13 11:54:05 +0200

    keyof

--- Generics/generics-01-starting-setup/src/app.ts
+++ Generics/generics-01-starting-setup/src/app.ts
@@ -4,5 +4,9 @@ function combine<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 
-let result = combine<{ name: string }, { age: number }>({ name: 'Ivo' }, {age:30});
-console.log(result)
+let result = combine<{ name: string }, { age: number }>({ name: 'Ivo' }, { age: 30 });
+console.log(result);
+
+function getData<T extends object, U extends keyof T>(obj: T, key: U) {
+    return obj[key]
+}
