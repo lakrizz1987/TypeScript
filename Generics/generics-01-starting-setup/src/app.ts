5a2071983e85243890db34b681c57c301fe5fbe7
Ivaylo <ignatov.ivaylo@gmail.com>
2022-12-13 12:05:55 +0200

    classes and generics

--- Generics/generics-01-starting-setup/src/app.ts
+++ Generics/generics-01-starting-setup/src/app.ts
@@ -10,3 +10,25 @@ console.log(result);
 function getData<T extends object, U extends keyof T>(obj: T, key: U) {
     return obj[key]
 }
+
+const data = getData({ name: 'Pesho' }, 'name');
+console.log(data);
+
+class DataStorage<T> {
+    private data: T[] = []
+
+    addItem(item: T) {
+        this.data.push(item)
+    }
+
+    print() {
+        
+        return this.data
+    }
+}
+
+const numbers = new DataStorage<string>;
+//numbers.addItem(2)
+numbers.addItem('Boy');
+numbers.addItem('Girl');
+console.log(numbers.print())
